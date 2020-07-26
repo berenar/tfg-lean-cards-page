39d7705980ccb6b7ff87e38d61f96dae2150b196
berenar <bernatpericasserra97@gmail.com>
2020-07-26 14:14:19 +0200

    Updates

---
+++ precache-manifest.a2f654fbaf610063745ca4869e8a9b06.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10c371beb18cee6228f3d335975bae0",
+    "url": "/tfg-lean-cards-page/index.html"
+  },
+  {
+    "revision": "335b53d49a99890cd602",
+    "url": "/tfg-lean-cards-page/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "e002ee60c22e5c102c86",
+    "url": "/tfg-lean-cards-page/static/css/main.9591c822.chunk.css"
+  },
+  {
+    "revision": "335b53d49a99890cd602",
+    "url": "/tfg-lean-cards-page/static/js/2.6ca2fdff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tfg-lean-cards-page/static/js/2.6ca2fdff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e002ee60c22e5c102c86",
+    "url": "/tfg-lean-cards-page/static/js/main.36cda047.chunk.js"
+  },
+  {
+    "revision": "68fbc4213b0011757b52",
+    "url": "/tfg-lean-cards-page/static/js/runtime-main.65bab913.js"
+  },
+  {
+    "revision": "ebcc842df38eb56dfdd41ecca475fdee",
+    "url": "/tfg-lean-cards-page/static/media/5 S pic.ebcc842d.png"
+  },
+  {
+    "revision": "a783ff4327f7d39d588f5316a2575ce0",
+    "url": "/tfg-lean-cards-page/static/media/5 S.a783ff43.pdf"
+  },
+  {
+    "revision": "a2cf666fc68fbc62f04447a28aa7b2b1",
+    "url": "/tfg-lean-cards-page/static/media/5 W + 2 H Examples pic.a2cf666f.png"
+  },
+  {
+    "revision": "3c2f86fc4aba57d3002f7c3317468589",
+    "url": "/tfg-lean-cards-page/static/media/5 W + 2 H Examples.3c2f86fc.pdf"
+  },
+  {
+    "revision": "d98671db944bc028dfc7e2bd1b16e1d1",
+    "url": "/tfg-lean-cards-page/static/media/5 W + 2 H pic.d98671db.png"
+  },
+  {
+    "revision": "1feca14929260ce2b214866c11fde695",
+    "url": "/tfg-lean-cards-page/static/media/5 W + 2 H.1feca149.pdf"
+  },
+  {
+    "revision": "8a326d44dfe58aa0e82b68efb666cadd",
+    "url": "/tfg-lean-cards-page/static/media/7 Mudas pic.8a326d44.png"
+  },
+  {
+    "revision": "ed9336958626124819c1798e7064b4a3",
+    "url": "/tfg-lean-cards-page/static/media/7 Mudas.ed933695.pdf"
+  },
+  {
+    "revision": "e74445bb3b59d5aa98fbe8dfe8d89658",
+    "url": "/tfg-lean-cards-page/static/media/JIT pic.e74445bb.png"
+  },
+  {
+    "revision": "b682a1f22e3917dab54499f00ad89241",
+    "url": "/tfg-lean-cards-page/static/media/JIT.b682a1f2.pdf"
+  },
+  {
+    "revision": "f7a1e269220b8ca118a7b5e01e645bba",
+    "url": "/tfg-lean-cards-page/static/media/Jidoka pic.f7a1e269.png"
+  },
+  {
+    "revision": "62264786113b9e491d458db7bbbbeb9d",
+    "url": "/tfg-lean-cards-page/static/media/Jidoka.62264786.pdf"
+  },
+  {
+    "revision": "80b18d4b0f228e69dcb4269d54b85d83",
+    "url": "/tfg-lean-cards-page/static/media/Kaizen pic.80b18d4b.png"
+  },
+  {
+    "revision": "c3715952d193422453d8a25a06c58c25",
+    "url": "/tfg-lean-cards-page/static/media/Kaizen.c3715952.pdf"
+  },
+  {
+    "revision": "1ac096d34619583f99351bdc339f4ffa",
+    "url": "/tfg-lean-cards-page/static/media/Kanban pic.1ac096d3.png"
+  },
+  {
+    "revision": "ee644b9057a5aa386acdacee9dd19cc9",
+    "url": "/tfg-lean-cards-page/static/media/Kanban.ee644b90.pdf"
+  },
+  {
+    "revision": "a20ccad990dbba3913b3ea3e807a8631",
+    "url": "/tfg-lean-cards-page/static/media/Lean-project-management.a20ccad9.jpg"
+  },
+  {
+    "revision": "1ea8678a88d106b75c4e4c144985a4cd",
+    "url": "/tfg-lean-cards-page/static/media/Logo_noback.1ea8678a.png"
+  },
+  {
+    "revision": "1b625a44be7f26538a29c1ab9ca41b10",
+    "url": "/tfg-lean-cards-page/static/media/Poka-Yoke pic.1b625a44.png"
+  },
+  {
+    "revision": "c7a03914403af61753e654d8cab7f4b2",
+    "url": "/tfg-lean-cards-page/static/media/Poka-Yoke.c7a03914.pdf"
+  },
+  {
+    "revision": "e5f702ddfde42c1d87098e166ed97837",
+    "url": "/tfg-lean-cards-page/static/media/Pull or Push pic.e5f702dd.png"
+  },
+  {
+    "revision": "c3662bd60e9f1163ebf80c5ba8d60251",
+    "url": "/tfg-lean-cards-page/static/media/Pull or Push.c3662bd6.pdf"
+  },
+  {
+    "revision": "63dd85f1559783ce8afbf8d8e52ee441",
+    "url": "/tfg-lean-cards-page/static/media/VSM pic.63dd85f1.png"
+  },
+  {
+    "revision": "b7d2950aff2dfdd2a94d32656434764a",
+    "url": "/tfg-lean-cards-page/static/media/VSM.b7d2950a.pdf"
+  },
+  {
+    "revision": "a2520ea6ac5c32b6d7cf667475bdc1d9",
+    "url": "/tfg-lean-cards-page/static/media/photo_me.a2520ea6.jpg"
+  }
+]);
