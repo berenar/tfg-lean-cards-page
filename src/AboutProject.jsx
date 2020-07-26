b6c46f90a94ed91c0f1b426f685041c92562945d
berenar <bernatpericasserra97@gmail.com>
2020-07-26 14:00:01 +0200

    styled navbar

--- src/AboutProject.jsx
+++ src/AboutProject.jsx
@@ -5,20 +5,21 @@ import Flip from 'react-reveal/Flip';
 import { MailTwoTone, GithubOutlined } from '@ant-design/icons';
 import { Link } from "@reach/router"
 
-function AboutProject() {
 
+function AboutProject() {
     return (
         <div className="AboutProject_container">
             <div className="centered">
                 <h1 className="big">About the project</h1>
-                <h3 className="margins"> This web is part of the end-of-degree in<br />
-                    Computer Science of
-                    <Link to="/about_author"> the author</Link>
-                    .
-                </h3>
+                <h3 className="margins"> This web is part of the end-of-degree project in<br />
+                    Computer Science about Lean Cards.<br /><br />
+                    They will be used to instruct software-related business how to apply <br />
+                    Lean methodologies to create more value for customers with fewer resources.
+                    </h3>
 
                 <Flip top cascade>
-                    <img id="cardExample" src={require("./assets/cards/images/_5s.jpg")} alt="card example"
+                    <img id="lean-keywords" src={require("./assets/Lean-project-management.jpg")}
+                        alt="lean keywords"
                     />
                 </Flip>
                 <div className="icons-list">
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,17 +6,22 @@ import { Link } from "@reach/router"
 import { Layout, Menu } from 'antd';
 const { Header } = Layout;
 
-function Navbar(props) {
+function Navbar() {
     return (
         <Layout className="layout">
-            <Header>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-                    <Menu.Item key="2"><Link to="cards_list">Cards</Link></Menu.Item>
-                    <Menu.Item key="3"><Link to="about_project">Project</Link></Menu.Item>
-                    <Menu.Item key="4"><Link to="about_author">Author</Link></Menu.Item>
-                </Menu>
-            </Header>
+            <Menu mode="horizontal" defaultSelectedKeys={['1']} className="nav-bar">
+                <Menu.Item key="1">
+                    <Link to="/">
+                        <img src={require("./assets/Logo_no_text_white.png")}
+                            alt=""
+                            className="mini-logo" 
+                            />
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="2"><Link to="cards_list">Cards</Link></Menu.Item>
+                <Menu.Item key="3"><Link to="about_project">Project</Link></Menu.Item>
+                <Menu.Item key="4"><Link to="about_author">Author</Link></Menu.Item>
+            </Menu>
         </Layout>
     )
 }
